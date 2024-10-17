9f22354d98dc57c1dcda312dc4af65b7c28cfa63
Jake <jacob.watson00@yahoo.com>
2024-10-17 16:06:03 -0600

    connected to postgres, ran schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50)
-)
+);
 
 CREATE TABLE Role (
     id SERIAL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL NOT NULL,
     description_id INTEGER NOT NULL
-)
+);
 
 CREATE TABLE Employee (
     id SERIAL PRIMARY KEY,
@@ -16,4 +16,4 @@ CREATE TABLE Employee (
     last_name VARCHAR(30),
     role_id INTEGER NOT NULL,
     manager_id INTEGER NOT NULL
-)
+);
